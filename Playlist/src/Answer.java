7ba5014552d5916d081285025df81024394cf59a
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-01-13 04:23:17 +0300

    Playlist coding exercise

---
+++ Playlist/src/Answer.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Answer {
+    // write code here
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
+
+    public Answer(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new ArrayList<Song>();
+    }
+
+    public boolean addSong(String songTitle, double duration) {
+        // if song added
+
+        if (findSong(songTitle) == null) {
+            songs.add(new Song(songTitle, duration));
+            return true;
+        }
+
+        return false;
+    }
+
+    private Song findSong(String songTitle) {
+        // if song exists
+        for (var track : songs) {
+            if (track.getTitle().equals(songTitle)) {
+                return track;
+            }
+        }
+
+        return null; // if not
+    }
+
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playlist) {
+        // if it exists and was added successfuly, true
+        int index = trackNumber - 1;
+
+        if ((index >= 0) && (index <= songs.size())) {
+            playlist.add(songs.get(index));
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean addToPlayList(String songTitle, LinkedList<Song> playlist) {
+        // if it exists and was added successfuly, true
+        var track = findSong(songTitle);
+        if (track != null) {
+            playlist.add(track);
+            return true;
+        }
+
+        return false;
+    }
+}
