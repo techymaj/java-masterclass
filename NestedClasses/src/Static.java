43e6ff29777727834d314cb7d1c25184b8b835e0
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-01-23 01:11:52 +0300

    Static Inner Classes can access public & private inner class members. can also access public & private outer class members

---
+++ NestedClasses/src/Static.java
@@ -0,0 +1,27 @@
+public class Static {
+
+    public int age;
+    private int year;
+
+    public static void main(String[] args) {
+        // can access public & private inner class members
+        Static.Inner inner = new Inner();
+        inner.name = "Hank";
+        inner.value = "Val";
+        System.out.println(inner.name + " " + inner.value);
+
+    }
+
+    // static inner class, declared in class body
+    static class Inner {
+        String name = "jax";
+        private String value = "Values";
+
+        public static void main(String[] args) {
+            // can access private & public outer class members
+            Static stat = new Static();
+            int sum = stat.age + stat.year;
+            System.out.println(sum);
+        }
+    }
+}
