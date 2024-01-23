1aad4d8eb83c474196c521580199b95cceb9b1d6
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-01-23 18:57:09 +0300

    Effectively Final Method Local Variables

--- NestedClasses/src/MethodLocalClass.java
+++ NestedClasses/src/MethodLocalClass.java
@@ -2,18 +2,24 @@ public class MethodLocalClass {
     final int age = 29;
 
     void methodLocal(final int aged) {
+        int effectivelyFinal = 0;
         class M {
             // this variable is captured
             // meaning: stored with the instance
             final int age;
+
              M(int age) {
                  this.age = age;
              }
 
              void desc() {
+                 // won't compile. effectivelyFinal is effectively final
+//                 effectivelyFinal = 50;
+
                  System.out.println("You are " + this.age + " years old");
                  System.out.println("Someone else is " + aged);
                  System.out.println("And another " + MethodLocalClass.this.age);
+                 System.out.println("You can't be " + effectivelyFinal + " years old");
              }
         }
 
