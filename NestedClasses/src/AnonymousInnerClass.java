abb06b3474010ebe855cd3f65aff7aad7755efd8
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-01-23 20:14:07 +0300

    Anon inner classes are useful during abstraction or if you only want to create one instance of the class

--- NestedClasses/src/AnonymousInnerClass.java
+++ NestedClasses/src/AnonymousInnerClass.java
@@ -16,6 +16,25 @@ public class AnonymousInnerClass {
             }
         };
         nan.define();
+
+        Yo y = new Yo();
+        y.define("JA");
+
+        Yo yo = new Yo() {
+            @Override
+            public void define(String name) {
+                System.out.println("Yo, still anon, " + name);
+            }
+        };
+        yo.define("JAX");
+
+        Alo alo = new Alo() {
+            @Override
+            public void define(String s) {
+                System.out.println("Alo...");
+            }
+        };
+        alo.define("..");
     }
 }
 
@@ -28,3 +47,16 @@ class StillAnon {
         System.out.println("I AM");
     }
 }
+
+interface IAnon {
+    void define(String s);
+}
+
+class Yo implements IAnon {
+    @Override
+    public void define(String name) {
+        System.out.println("Yo, still anon, y'all");
+    }
+}
+
+abstract class Alo implements IAnon {}
