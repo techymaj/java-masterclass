a185df0f249d2c4cb1521e120fc60c9f2ec4c3e3
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-01-29 02:09:29 +0300

    Arrays is a helper class

---
+++ Arrays/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] array = new int[]{1, 2, 3, 4, 5};
+        Arrays.setAll(array, i -> array[i] * 2); // in place
+        System.out.println(array); // [2, 4, 6, 8, 10]
+        System.out.println(Arrays.toString(doubleArray(array))); // [4, 8, 12, 16, 20]
+    }
+
+    public static int[] doubleArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array[i] * 2;
+        }
+        return array;
+    }
+}
