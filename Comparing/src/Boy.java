a7cb8bb232ad2df8b96e9962927f21acc404571e
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-01-29 04:12:17 +0300

    Type erasure in action

---
+++ Comparing/src/Boy.java
@@ -0,0 +1,3 @@
+public class Boy extends Human {
+
+}
--- Comparing/src/CompareTo.java
+++ Comparing/src/CompareTo.java
@@ -12,8 +12,17 @@ public class CompareTo implements Comparable<String> {
         System.out.println(result);
     }
 
+    // in this case, due to type erasure, String is erased and replaced with Object
+    // because Comparable<String extends java.lang.Object> implicitly
+//    @Override
+//    public int compareTo(Object o) {
+//        return 0;
+//    }
+
     @Override
     public int compareTo(String o) {
         return a.compareTo(o);
     }
+
 }
+
