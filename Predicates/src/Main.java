9819ecbe39c766333d396c4838a5fc0d785fd055
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-01-30 02:30:24 +0300

    Predicates And Comparing

--- Predicates/src/Main.java
+++ Predicates/src/Main.java
@@ -1,17 +1,28 @@
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.function.Predicate;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String[] names = {"John", "Jane", "Jack", "Jill"};
+        String[] names = {"John Mason", "Jane Foster", "Jack Ma", "Jill"};
+
+        findHer(names);
+
+        var namesList = Arrays.asList(names);
+        namesList.sort(Comparator.comparing(String::length));
+        System.out.println(namesList);
+        namesList.forEach(System.out::println);
+    }
+
+    public static void findHer(String[] names) {
+        int iteration = 1;
         Predicate<String> firstLetter;
         Predicate<String> endLetter;
-
-        int iteration = 1;
         for (var name : names) {
             firstLetter = s -> s.startsWith("Ja");
-            endLetter = s -> s.endsWith("e");
+            endLetter = s -> s.endsWith("r");
             if (firstLetter.and(endLetter).test(name)) {
                 System.out.println("Found " + name + "! after " + iteration + " iterations.");
                 return;
