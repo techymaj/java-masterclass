8cde1207bd5a01a7e8d297af7f978ee232a28542
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-02-03 22:08:41 +0300

    Added thought(pause) to AI

--- TheCardGameChallenge/src/tech/majaliwa/AI.java
+++ TheCardGameChallenge/src/tech/majaliwa/AI.java
@@ -1,5 +1,10 @@
 package tech.majaliwa;
 
+import java.util.Random;
+import java.util.logging.Level;
+
+import static java.util.logging.Logger.*;
+
 public class AI extends User {
 
     public AI(String name) {
@@ -19,8 +24,8 @@ public class AI extends User {
             }
         }
 
+        randomizeThought();
         // If no valid card found in hand, pick a card from the deck
-        System.out.println("AI is thinking...");
         var pickedCard = UserInterface.pickCard(Game.deck);
 
         if (pickedCard == null) {
@@ -38,5 +43,40 @@ public class AI extends User {
         }
 
         return null; // pass turn
+    }
+
+    private static void aiIsThinking() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            getLogger(AI.class.getName()).log(Level.INFO, e.getMessage(), e);
+        }
+    }
+
+    public static void randomizeThought() {
+        Random random = new Random();
+        int thought = random.nextInt(10);
+        switch (thought) {
+            case 1 -> {
+                System.out.println("Hold on, I'm thinking...");
+                aiIsThinking();
+            }
+            case 2 -> {
+                System.out.println("I am so winning this...");
+                aiIsThinking();
+            }
+            case 3 -> {
+                System.out.println("Strategizing...");
+                aiIsThinking();
+            }
+            case 4 -> {
+                System.out.println("Wait, i am planning...");
+                aiIsThinking();
+            }
+            default -> {
+                System.out.println("I'm starting to think you might win this one...");
+                aiIsThinking();
+            }
+        }
     }
 }
