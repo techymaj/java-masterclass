0d18723fc7194b86a5c73c305211f5c0a2a3b3fa
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-02-12 13:40:35 +0300

    Set Operations
    - Union set | addAll()
    - Intersection set | retainAll()
    - Difference Set | removeAll()

--- SetOperations/src/Main.java
+++ SetOperations/src/Main.java
@@ -6,6 +6,22 @@ public class Main {
 
     public static void main(String[] args) {
 
+        var champions = unionSet();
+        System.out.println("--- Union Set ---");
+        System.out.println(champions);
+        System.out.println();
+
+        var intersection = intersectionSet();
+        System.out.println("--- Intersection Set ---");
+        System.out.println(intersection);
+        System.out.println();
+
+        var difference = difference();
+        System.out.println("--- Difference Set ---");
+        System.out.println(difference);
+    }
+
+    private static Set<String> unionSet() {
         Set<String> epl = new HashSet<>();
         epl.add("Arsenal");
         epl.add("Chelsea");
@@ -22,25 +38,50 @@ public class Main {
 
         Set<String> champions = new HashSet<>(epl);
         // using addAll() method to form a Union set
-//        champions.addAll(epl);
-//        champions.addAll(ucl);
+        champions.addAll(epl);
+        champions.addAll(ucl);
+        return champions;
+    }
 
-//        System.out.println(champions);
+    private static Set<String> intersectionSet() {
+        Set<String> epl = new HashSet<>();
+        epl.add("Arsenal");
+        epl.add("Chelsea");
+        epl.add("Manchester United");
+        epl.add("Manchester City");
+        epl.add("Liverpool");
+        epl.add("Tottenham Hotspur");
 
-        System.out.println("-----------------");
-        // retainAll() method to form an intersection set
+        Set<String> ucl = new HashSet<>();
+        ucl.add("Real Madrid");
+        ucl.add("Barcelona");
+        ucl.add("Arsenal");
+        ucl.add("Manchester City");
+
+        // using retainAll() method to form an intersection set
         // in the epl set, retain only elements present in both the epl and ucl set
         epl.retainAll(ucl);
+        return epl;
+    }
 
-        System.out.println(epl);
-        System.out.println(ucl);
+    private static Set<String> difference() {
+        Set<String> epl = new HashSet<>();
+        epl.add("Arsenal");
+        epl.add("Chelsea");
+        epl.add("Manchester United");
+        epl.add("Manchester City");
+        epl.add("Liverpool");
+        epl.add("Tottenham Hotspur");
 
-        System.out.println("-----------------");
-        // removeAll() method to form a difference set
-        // in the ucl set, remove everything that is in the epl set
-        ucl.removeAll(epl);
+        Set<String> ucl = new HashSet<>();
+        ucl.add("Real Madrid");
+        ucl.add("Barcelona");
+        ucl.add("Arsenal");
+        ucl.add("Manchester City");
 
-        System.out.println(epl);
-        System.out.println(ucl);
+        // using removeAll() method to form a difference set
+        // in the epl set, remove all elements present in the ucl set
+        epl.removeAll(ucl);
+        return epl;
     }
 }
