448315feadb00e8d12c44660e05a0c11858e7209
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-02-12 05:26:20 +0300

    retainAll() in Sets

--- SetOperations/src/Main.java
+++ SetOperations/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,14 +17,22 @@ public class Main {
         Set<String> ucl = new HashSet<>();
         ucl.add("Real Madrid");
         ucl.add("Barcelona");
-        epl.add("Arsenal");
-        epl.add("Manchester City");
+        ucl.add("Arsenal");
+        ucl.add("Manchester City");
 
         Set<String> champions = new HashSet<>(epl);
         // using addAll() method to form a Union set
-        champions.addAll(epl);
-        champions.addAll(ucl);
+//        champions.addAll(epl);
+//        champions.addAll(ucl);
 
-        System.out.println(champions);
+//        System.out.println(champions);
+
+        System.out.println("-----------------");
+        // retainAll() method to form an intersection set
+        // in the epl set, retain only elements present in both the epl and ucl set
+        epl.retainAll(ucl);
+
+        System.out.println(epl);
+        System.out.println(ucl);
     }
 }
