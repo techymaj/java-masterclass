e74bea2b2f52dece345af72c94ec7a15512bde22
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-02-16 15:19:24 +0300

    TDD with Stack

---
+++ StackTDD/src/Stack.java
@@ -0,0 +1,20 @@
+public class Stack {
+    private int size = 0;
+    private int[] elements = new int[2];
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void push(int element) {
+        elements[size++] = element;
+    }
+
+    public int pop() {
+        if (isEmpty()) throw new Underflow();
+        return elements[--size];
+    }
+
+    public class Underflow extends RuntimeException {
+    }
+}
