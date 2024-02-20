b440b5e74f5103773b6a658d0b72d89cf13c2ce1
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-02-20 18:51:57 +0300

    Abstraction example

---
+++ AbstractionDemo/src/God.java
@@ -0,0 +1,13 @@
+public abstract class God {
+    public abstract void doGodlyStuff();
+}
+class Angel extends God {
+    @Override
+    public void doGodlyStuff() {
+        System.out.println("I am an angel, I am here to protect you!");
+    }
+    public static void main(String[] args) {
+        God angel = new Angel();
+        angel.doGodlyStuff();
+    }
+}
