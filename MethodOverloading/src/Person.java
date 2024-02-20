3159c9eaec14ebeecd1bfcf42a9661d5afd418b3
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-02-20 12:43:33 +0300

    OVerloading and Overriding methods

--- MethodOverloading/src/Person.java
+++ MethodOverloading/src/Person.java
@@ -1,16 +1,22 @@
 public class Person {
 
-    public static void main(String[] args) {
-        Person me = new Person();
-        me.describe("Andrew");
-        me.describe("Andrew", 21);
-    }
-
+    // Create a method that takes in a name and prints it
     public void describe(String name) {
         System.out.println("My name is " + name);
     }
 
-    public void describe(String name, int age) {
-        System.out.println("My name is " + name + " and I am " + age);
+    // Create a method that takes in a name and age and prints it
+    public String describe(String name, int age) {
+        return "My name is " + name + " and I am " + age;
+    }
+
+    public static void main(String[] args) {
+        // Create an instance of the Person class
+        Person me = new Person();
+        // Call the describe method with a name
+        me.describe("Andrew");
+        // Call the describe method with a name and age
+        String iAm = me.describe("Andrew", 21);
+        System.out.println(iAm);
     }
 }
---
+++ MethodOverriding/src/Animal.java
@@ -0,0 +1,23 @@
+public class Animal {
+
+    public void eat() {
+        System.out.println("Animals eat");
+    }
+}
+
+// This is inheritance (Is-A relationship)
+// A Human Is-A Animal
+class Human extends Animal {
+    @Override
+    public void eat() {
+        // super.eat() calls the method in the parent class which is Animal
+        // you can choose to not call super.eat()
+        super.eat(); // This calls the eat() method in the Animal class
+        System.out.println("Humans eat");
+    }
+
+    public static void main(String[] args) {
+        Human human = new Human();
+        human.eat(); // This calls the eat() method in the Human class
+    }
+}
