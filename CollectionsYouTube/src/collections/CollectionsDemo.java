c6ef24cb6492cc6993ed6cd28e3258e7c44854f3
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-07-23 17:58:25 +0300

    CollectionsDemo

---
+++ CollectionsYouTube/src/collections/CollectionsDemo.java
@@ -0,0 +1,43 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Stream;
+
+public class CollectionsDemo {
+
+    public static void show() {
+        Collection<String> collection = new ArrayList<>();
+        collection.add("I");
+        collection.add("Am");
+        collection.add("Number");
+        collection.add("Four");
+        Collections.addAll(collection, "5", "4", "3", "2", "1");
+
+        Collection<String> other = new ArrayList<>(collection);
+
+        System.out.println(other == collection); // false
+        System.out.println(other.equals(collection)); // true
+
+        var stringArr = other.toArray(new String[0]);
+        Stream.of(stringArr)
+                .filter(CollectionsDemo::isNumber)
+                .forEach(System.out::println);
+
+        System.out.println("========== Iterator ==========");
+        var iterator = collection.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    static boolean isNumber(String num) {
+        try {
+            Integer.parseInt(num);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
---
+++ CollectionsYouTube/src/collections/Main.java
@@ -0,0 +1,9 @@
+package collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        CollectionsDemo.show();
+    }
+}
