0bcf350bf5e1079264f17d0aa4879ba8b54283b2
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-08-08 11:37:29 +0300

    Replace (SIZE / 8) with the BYTES constant

--- ClientServer/src/Main.java
+++ ClientServer/src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        
-        System.out.println("Size of byte: " + (Byte.SIZE / 8) + " bytes.");
-        System.out.println("Size of short: " + (Short.SIZE / 8) + " bytes.");
-        System.out.println("Size of int: " + (Integer.SIZE / 8) + " bytes.");
-        System.out.println("Size of long: " + (Long.SIZE / 8) + " bytes.");
-        System.out.println("Size of char: " + (Character.SIZE / 8) + " bytes.");
-        System.out.println("Size of float: " + (Float.SIZE / 8) + " bytes.");
-        System.out.println("Size of double: " + (Double.SIZE / 8) + " bytes.");
+
+        System.out.println("Size of byte: " + (Byte.BYTES) + " bytes.");
+        System.out.println("Size of short: " + (Short.BYTES) + " bytes.");
+        System.out.println("Size of int: " + (Integer.BYTES) + " bytes.");
+        System.out.println("Size of long: " + (Long.BYTES) + " bytes.");
+        System.out.println("Size of char: " + (Character.BYTES) + " bytes.");
+        System.out.println("Size of float: " + (Float.BYTES) + " bytes.");
+        System.out.println("Size of double: " + (Double.BYTES) + " bytes.");
     }
 }
